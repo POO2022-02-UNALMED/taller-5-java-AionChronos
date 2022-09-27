2a73e54afe077ce7cb76fdbc472d899ed58a7c97
Julián Orrego Martínez <70676228+AionChronos@users.noreply.github.com>
2022-09-27 00:05:28 -0500

    a

---
+++ src/main/java/zooAnimales/Animal.java
@@ -0,0 +1,93 @@
+package zooAnimales;
+import java.util.ArrayList;
+
+import gestion.Zona;
+
+public class Animal {
+    private static int totalAnimales;
+    private String nombre;
+    private int edad;
+    private String habitat;
+    private String genero;
+    private ArrayList<Zona> zona = new ArrayList<Zona>();
+
+    public Animal(String nombre, int edad, String habitat, String genero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.genero = genero;
+		this.habitat = habitat;
+		totalAnimales++;
+	}
+	
+	public Animal() {
+		this(null, 0, null, null);
+	}
+	
+	public int getTotalAnimales() {
+		return totalAnimales;
+	}
+
+	public void setTotalAnimales(int totalAnimales) {
+		this.totalAnimales = totalAnimales;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public ArrayList<Zona> getZona() {
+		return zona;
+	}
+
+	public void setZona(ArrayList<Zona> zona) {
+		this.zona = zona;
+	}
+
+	public String movimiento() {
+		return "desplazarse";
+	}
+	
+	public static String totalPorTipo() {
+        return String.format("Mamiferos: %d\nAves: %d\nReptiles: %d\nPeces: %d\nAnfibios: %d", Mamifero.cantidadMamiferos(), Ave.cantidadAves(), Reptil.cantidadReptiles(), Pez.cantidadPeces(), Anfibio.cantidadAnfibios());
+    }
+	
+	@Override
+    public String toString() {
+        if (zona.size() == 0) {
+            return "Mi nombre es " + nombre + ", tengo una edad de " + edad + ", habito en " + habitat + " y mi genero es " + genero;
+        }
+		else{
+			return "Mi nombre es " + nombre + ", tengo una edad de " + edad + ", habito en " + habitat + " y mi genero es " + genero +
+                    ", la zona en la que me ubico es " + zona + ", en el " + zona.get(0).getZoo().getNombre();
+		}
+    }
+
+}
---
+++ src/main/java/zooAnimales/Reptil.java
@@ -0,0 +1,70 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+
+public class Reptil extends Animal {
+	private static ArrayList<Reptil> listado = new ArrayList<Reptil>();
+	public static int iguanas;
+	public static int serpientes;
+	private String colorEscamas;
+	private int largoCola;
+	
+	public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
+		super(nombre, edad, habitat, genero);
+		this.colorEscamas = colorEscamas;
+		this.largoCola = largoCola;
+		Reptil.listado.add(this);
+	}
+	
+	public Reptil() {
+		this(null, 0, null, null, null, 0);
+	}
+	
+	public ArrayList<Reptil> getListado() {
+		return Reptil.listado;
+	}
+
+	public void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado = listado;
+	}
+
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+
+	public int getLargoCola() {
+		return largoCola;
+	}
+
+	public void setLargoCola(int largoCola) {
+		this.largoCola = largoCola;
+	}
+
+	public static int cantidadReptiles() {
+		return Reptil.listado.size();
+	}
+	
+	public String movimiento() {
+		return "reptar";
+		
+	}
+	
+	public static Reptil crearIguana(String nombre, int edad, String genero) {
+		Reptil iguana = new Reptil(nombre, edad, "humedal", genero, "verde", 3);
+		iguanas++;
+		return iguana;
+
+	}
+	
+	public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+		Reptil serpiente = new Reptil(nombre, edad, "jungla", genero, "blanco", 1);
+		serpientes++;
+		return serpiente;
+
+	}
+}
+
